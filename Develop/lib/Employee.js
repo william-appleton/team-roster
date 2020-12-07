20edf891f59a3407f9cdf7024a64e92523e8f5f4
Imprudent Friend <ImprudentFriend@gmail.com>
2020-12-07 07:26:20 -0500

    added first employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+
+export default class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getID() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+    
+    getRole() {
+        return 'Employee'
+    }
+}
+
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,7 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+import Employee from "./Employee";
+
+Alice = new Employee("Alice", "HDHD", "a@aaaa")
+
+console.log(Alice)
